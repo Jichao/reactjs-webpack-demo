f3c4ee0c9a1d49ec2f3d2e33bff696eb153fd71b
jichaoyang <jcyangzh@gmail.com>
2016-08-12 16:28:04 +0800

    add node_env and sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
+var webpack = require('webpack');
 module.exports = {
-    entry: "./app.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel', // 'babel-loader' is also a legal name to reference
-            query: {
-              presets: ['es2015', 'react']
-            }
-          }
-        ]
-    }
+  entry: "./app.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]
 };
